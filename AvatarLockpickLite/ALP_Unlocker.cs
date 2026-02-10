11fa213cc3e61bd0044151def1dcff9df7d5d520
Scrim <105956702+scrim-dev@users.noreply.github.com>
2026-02-10 13:14:02 -0400

    To do.

---
+++ AvatarLockpickLite/ALP_Unlocker.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace AvatarLockpickLite
+{
+    internal class ALP_Unlocker
+    {
+        //Placeholder for now
+        public static void UnlockAvatar(string userId, string avatarId)
+        {
+            MessageBox.Show($"Press OK to start unlock process of {avatarId} on {userId}", "ALP Unlocker");
+        }
+    }
+}
---
+++ AvatarLockpickLite/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Diagnostics;
+
+namespace AvatarLockpickLite
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TitleBar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void BtnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void BtnUnlock_Click(object sender, RoutedEventArgs e)
+        {
+            ALP_Unlocker.UnlockAvatar(TxtUserId.Text, TxtAvatarId.Text);
+        }
+
+        private void BtnRestart_Click(object sender, RoutedEventArgs e)
+        {
+            var process = Process.GetProcessesByName("VRChat").FirstOrDefault();
+            if (process != null)
+            {
+                process.Kill();
+                process.WaitForExit(5000);
+            }
+
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = "steam://rungameid/438100 --no-vr",
+                UseShellExecute = true
+            });
+        }
+    }
+}
