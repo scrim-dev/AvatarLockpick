e50d0a8f0ca828f0159f67c6f9fbd5269052c141
Scrim <105956702+scrim-dev@users.noreply.github.com>
2026-04-25 21:49:16 -0400

    Setup for linux build soon..

--- build.bat
+++ build.bat
@@ -1,6 +1,7 @@
 @echo off
 for %%f in (*.sln) do set "SOLUTION_FILE=%%f"
 dotnet build "%SOLUTION_FILE%" -c Release /p:Platform="Any CPU" /p:PlatformTarget=x64
+dotnet publish "%SOLUTION_FILE%" -c Release -r linux-x64 --self-contained true -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true -o linux_release
 timeout /t 5
 iscc "AvatarLockpick.Installer\InstallerScript.iss"
 pause
